6c60afc740a5351125ed409aedcd2ffd364fa99b
fabio.gm.simeoni@gmail.com <fabio.gm.simeoni@gmail.com@1603af65-4f32-2b72-4804-cea233bc64de>
2011-06-02 09:38:18 +0000

    with abstract LigoData implementations

---
+++ trunk/ligo-api/src/main/java/org/ligo/core/data/impl/AbstractLigoValue.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.ligo.core.data.impl;
+
+import org.ligo.core.data.LigoValue;
+
+/**
+ * @author Fabio Simeoni
+ *
+ */
+public abstract class AbstractLigoValue implements LigoValue {
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((get() == null) ? 0 : get().hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof LigoValue))
+			return false;
+		LigoValue other = (LigoValue) obj;
+		if (get() == null) {
+			if (other.get() != null)
+				return false;
+		} else if (!get().equals(other.get()))
+			return false;
+		return true;
+	}
+	
+	/**{@inheritDoc}*/
+	@Override
+	public String toString() {
+		return get().toString();
+	}
+	
+	
+}
